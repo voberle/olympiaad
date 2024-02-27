3db17772ec33383f0b7f4eb2560227439bc048f8
Vincent Oberle <vincent@oberle.org>
2024-02-27 11:59:46 +0200

    2023-24 Eelvoor/segane: Initial version for N=2

---
+++ 2023-24_eelvoor/segane/README.md
@@ -0,0 +1,63 @@
+# segane
+
+Exercise 6 of Eesti informaatikaolümpiaad 2023-2024 Eelvoor.
+
+[Original description in Estonian](https://eio.ee/uploads/Main/2023-12-10-ev-et.zip).
+
+## Translation
+
+Confusing Output (confusing) - 3 sec / 10 sec 60 points
+
+Juku experimented with various command-line programs. When running most programs multiple times in a row, he got the same output multiple times. For example, the pwd program produced the following when run twice in a row:
+
+    /home/juku/lahendused/segane
+    /home/juku/lahendused/segane
+
+However, such output is guaranteed only when the programs are executed sequentially. When Juku launched multiple instances of one program simultaneously, he noticed that their outputs intertwined, resulting in a different outcome with each attempt. For instance, when running pwd with two instances simultaneously, Juku got the following outputs on two occasions:
+
+    /home//homjukue/ju/lahkeundu/sedl/ahseeganendused/segane
+    /h/ohomme/e/juku/lajukhue/landusehedn/segadunseed/segane
+
+Juku noticed that although the characters output by different instances of the program might interleave, the entire output of each instance was complete, and all characters output by one instance were in the correct order.
+
+Juku decided to repeatedly experiment with this strange phenomenon, but after some time trying, he realized that he actually didn't know the output of some programs. Now, based on the existing information, he wants to reconstruct the possible outputs.
+
+*Input.* In this task, the input may consist of multiple subtests. The first line of the input specifies the number of subtests T (1 ⩽ T ⩽ 20).
+
+Each subtest consists of two lines. The first line contains the number of executed programs N (2 ⩽ N ⩽ 10). The second line contains a string S, the output obtained from running N instances of one program simultaneously. For simplicity, this string consists only of lowercase Latin letters and does not contain spaces, line breaks, or other special characters.
+
+*Output.* For each subtest, output two lines. The first line should contain an integer indicating the number of all possible outputs of the program. The second line should contain all possible outputs of the program separated by spaces and in alphabetical order.
+
+Let L be the length of the output of one instance of the program (the length of S is then N · L). For each subtest, it is guaranteed that N ^ L ⩽ 2 · 10^7. The following table shows the maximum values of L corresponding to possible values of N:
+
+    N   max L
+    2   24
+    3   15
+    4   12
+    5   10
+    6   9
+    7   8
+    8   7
+    9   6
+    10  6
+
+*Example.*
+
+    Input                     Output
+    4                         1
+    2                         abcd
+    abcdabcd                  1
+    5                         aa
+    aaaaaaaaaa                2
+    2                         aabb abab
+    aabbaabb                  1
+    4                         error
+    eerrrroereorrrorrror
+
+In the first subtest, the outputs of program instances are output sequentially. In the second subtest, the program instances could output characters in many different ways, but there is only one possible variant for the program output. The third subtest illustrates a situation where the program could output one of two possible outputs.
+
+*Evaluation.* In this task, tests (inputs) are divided into groups. Points are awarded only to those solutions that pass all tests belonging to the group. The following additional conditions apply to the groups:
+
+1. (15 points) N = 2, L ⩽ 11.
+2. (15 points) N = 2.
+3. (30 points) No additional conditions.
---
+++ 2023-24_eelvoor/segane/src/main.rs
@@ -0,0 +1,134 @@
+use std::io::{self, BufRead};
+use std::collections::{HashSet, VecDeque};
+
+// Trying to solve it with N = 2
+fn find_output_n2(mut inq: VecDeque<char>, mut out1: Vec<char>, mut out2: Vec<char>, results: &mut HashSet<Vec<char>>, expected_len: usize) {
+    // println!("inq={:?}: {:?} {:?}", inq, out1, out2);
+    // println!("inq.len={}: o1={} o2={}, exp={}", inq.len(), out1.len(), out2.len(), expected_len);
+    
+    while let Some(first) = inq.pop_front() {
+        // The shortest part has to be equal to the beginning of the other part
+        if out1.len() < out2.len() {
+            if out1 != out2[0..out1.len()] {
+                return;
+            }
+        } else {
+            if out1[0..out2.len()] != out2 {
+                return;
+            }
+        }
+
+        // println!("{first}: {:?} {:?}", out1, out2);
+        let o1_len = out1.len();
+        let o2_len = out2.len();
+        // if we put it to the shortest, does it work? If not we can exclude that option
+        if o1_len < o2_len && o2_len < expected_len {
+            let pos = o1_len;
+            if out2[pos] != first {
+                // we cannot put the char into out1, has to go to out2.
+                out2.push(first);
+                continue;
+            }
+        } else if o1_len > o2_len && o1_len < expected_len {
+            let pos = o2_len;
+            if out1[pos] != first {
+                out1.push(first);
+                continue;
+            }
+        } else if o1_len == o2_len && o1_len < expected_len {
+            if out1 == out2 {
+                out1.push(first);
+                continue;
+            }
+        } else if o1_len == expected_len {
+            out2.push(first);
+            continue;        
+        } else if o2_len == expected_len {
+            out1.push(first);
+            continue;        
+        }
+
+        if inq.is_empty() {
+            break;
+        }
+
+        // any other case, we have two options, can go to either
+        if o1_len < expected_len {
+            let mut new_out1 = out1.clone();
+            new_out1.push(first);
+            find_output_n2(inq.clone(), new_out1, out2.clone(), results, expected_len);
+        }
+
+        if o2_len < expected_len {
+            let mut new_out2 = out2.clone();
+            new_out2.push(first);
+            find_output_n2(inq.clone(), out1.clone(), new_out2, results, expected_len);
+        }
+
+        break;
+    }
+
+    // once input is empty, results are only valid if they are equals
+    if out1 == out2 && out1.len() == expected_len {
+        // println!("Insert {:?}", out1);
+        results.insert(out1);
+    }
+
+}
+
+fn find_output(input: &str, count: usize) -> Vec<String> {
+    assert_eq!(count, 2);
+
+    let mut results: HashSet<Vec<char>> = HashSet::new();
+
+    let out1: Vec<char> = Vec::new();
+    let out2: Vec<char> = Vec::new();
+
+    let inq: VecDeque<char> = input.chars().collect();
+    let expected_len = inq.len() / 2;
+
+    find_output_n2(inq, out1, out2, &mut results, expected_len);
+    // println!("{:?}", results);
+
+    let mut res_str: Vec<String> = results.iter().map(|r| r.iter().collect()).collect();
+    res_str.sort_unstable();
+    res_str
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut it = stdin.lock().lines().skip(1);
+    let mut inputs: Vec<(usize, String)> = Vec::new();
+    while let Some(Ok(l)) = it.next() {
+        let count: usize = l.parse().unwrap();
+        let input = it.next().unwrap().unwrap();
+        inputs.push((count, input));
+    }
+
+    for input in inputs {
+        if input.0 != 2 {
+            continue;
+        }
+        let result = find_output(&input.1, input.0);
+        println!("{}", result.len());
+        println!("{}", result.iter().map(|v| v.to_string())
+        .collect::<Vec<String>>()
+        .join(" "));
+    }
+}
+
+#[test]
+fn test_find_output_n2() {
+    assert_eq!(find_output("/home//homjukue/ju/lahkeundu/sedl/ahseeganendused/segane", 2), ["/home/juku/lahendused/segane"]);
+    assert_eq!(find_output("/h/ohomme/e/juku/lajukhue/landusehedn/segadunseed/segane", 2), ["/home/juku/lahendused/segane"]);
+    assert_eq!(find_output("abcdabcd", 2), ["abcd"]);
+    assert_eq!(find_output("aabbaabb", 2), ["aabb", "abab"]);
+}
+
+#[test]
+fn test_other() {
+    assert_eq!(find_output("iiiiirirrrirrrirrrrrrr", 2), ["iiiirrrrrrr", "iiirirrrrrr", "iiirrirrrrr", "iiirrrirrrr"]);
+}
+
+    // assert_eq!(find_output("aaaaaaaaaa", 5), ["aa"]);
+    // assert_eq!(find_output("eerrrroereorrrorrror", 4), ["error"]);
